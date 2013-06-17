5a02657f616e7fd650e3271c7f936ae06723617c
chatree.srichart@gmail.com <chatree.srichart@gmail.com@7281f183-68a6-c996-e254-01dca9f16df4>
2013-06-17 16:25:14 +0000

    Add more widgets: - Accordion - AccordionGroup - Checkbox - Collapse - HorizontalRule

--- src/org/ofbiz/angularjs/widget/screen/AngularJsScreenWidget.java
+++ src/org/ofbiz/angularjs/widget/screen/AngularJsScreenWidget.java
@@ -23,6 +23,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.ofbiz.base.util.GeneralException;
+import org.ofbiz.base.util.UtilValidate;
 import org.ofbiz.base.util.UtilXml;
 import org.ofbiz.base.util.string.FlexibleStringExpander;
 import org.ofbiz.widget.screen.ModelScreen;
@@ -33,6 +34,78 @@ import org.w3c.dom.Element;
 public class AngularJsScreenWidget {
 
     public final static String module = AngularJsScreenWidget.class.getName();
+    
+    @SuppressWarnings("serial")
+    public static class Accordion extends ModelScreenWidget {
+        public static final String TAG_NAME = "accordion";
+        
+        protected FlexibleStringExpander closeOthersExdr;
+        protected List<ModelScreenWidget> subWidgets;
+
+        public Accordion(ModelScreen modelScreen, Element widgetElement) {
+            super(modelScreen, widgetElement);
+            this.closeOthersExdr = FlexibleStringExpander.getInstance(widgetElement.getAttribute("close-others"));
+            // read sub-widgets
+            List<? extends Element> subElementList = UtilXml.childElementList(widgetElement);
+            this.subWidgets = ModelScreenWidget.readSubWidgets(this.modelScreen, subElementList);
+        }
+
+        @Override
+        public void renderWidgetString(Appendable writer,
+                Map<String, Object> context,
+                ScreenStringRenderer screenStringRenderer)
+                throws GeneralException, IOException {
+            writer.append(this.rawString());
+            renderSubWidgetsString(this.subWidgets, writer, context, screenStringRenderer);
+            writer.append("</accordion>");
+        }
+
+        @Override
+        public String rawString() {
+            return "<accordion close-others=\"" + this.closeOthersExdr.getOriginal() + "\">";
+        }
+        
+    }
+    
+    @SuppressWarnings("serial")
+    public static class AccordionGroup extends ModelScreenWidget {
+        public static final String TAG_NAME = "accordion-group";
+        
+        protected FlexibleStringExpander headingExdr;
+        protected FlexibleStringExpander repeatExdr;
+        protected List<ModelScreenWidget> subWidgets;
+
+        public AccordionGroup(ModelScreen modelScreen, Element widgetElement) {
+            super(modelScreen, widgetElement);
+            this.headingExdr = FlexibleStringExpander.getInstance(widgetElement.getAttribute("heading"));
+            this.repeatExdr = FlexibleStringExpander.getInstance(widgetElement.getAttribute("repeat"));
+            // read sub-widgets
+            List<? extends Element> subElementList = UtilXml.childElementList(widgetElement);
+            this.subWidgets = ModelScreenWidget.readSubWidgets(this.modelScreen, subElementList);
+        }
+
+        @Override
+        public void renderWidgetString(Appendable writer,
+                Map<String, Object> context,
+                ScreenStringRenderer screenStringRenderer)
+                throws GeneralException, IOException {
+            writer.append(this.rawString());
+            renderSubWidgetsString(this.subWidgets, writer, context, screenStringRenderer);
+            writer.append("</accordion-group>");
+        }
+
+        @Override
+        public String rawString() {
+            StringBuilder builder = new StringBuilder();
+            builder.append("<accordion-group heading=\"" + this.headingExdr.getOriginal() + "\"");
+            if (UtilValidate.isNotEmpty(this.repeatExdr.getOriginal())) {
+                builder.append(" ng-repeat=\"" + this.repeatExdr.getOriginal() + "\"");
+            }
+            builder.append(">");
+            return builder.toString();
+        }
+        
+    }
     
     @SuppressWarnings("serial")
     public static class Application extends ModelScreenWidget {
@@ -72,11 +145,13 @@ public class AngularJsScreenWidget {
 
         protected FlexibleStringExpander textExdr;
         protected FlexibleStringExpander clickExdr;
+        protected FlexibleStringExpander styleExdr;
 
         public Button(ModelScreen modelScreen, Element widgetElement) {
             super(modelScreen, widgetElement);
             this.textExdr = FlexibleStringExpander.getInstance(widgetElement.getAttribute("text"));
             this.clickExdr = FlexibleStringExpander.getInstance(widgetElement.getAttribute("click"));
+            this.styleExdr = FlexibleStringExpander.getInstance(widgetElement.getAttribute("style"));
         }
 
         @Override
@@ -89,9 +164,75 @@ public class AngularJsScreenWidget {
 
         @Override
         public String rawString() {
-            return "<button ng-click=\"" + this.clickExdr.getOriginal() + "\">" + this.textExdr.getOriginal() + "</button>";
+            return "<button class=\"" + this.styleExdr.getOriginal() + "\" ng-click=\"" + this.clickExdr.getOriginal() + "\">" + this.textExdr.getOriginal() + "</button>";
         }
         
+    }
+    
+    @SuppressWarnings("serial")
+    public static class Checkbox extends ModelScreenWidget {
+        public static final String TAG_NAME = "checkbox";
+
+        protected FlexibleStringExpander textExdr;
+        protected FlexibleStringExpander modelExdr;
+        protected FlexibleStringExpander styleExdr;
+
+        public Checkbox(ModelScreen modelScreen, Element widgetElement) {
+            super(modelScreen, widgetElement);
+            this.textExdr = FlexibleStringExpander.getInstance(widgetElement.getAttribute("text"));
+            this.modelExdr = FlexibleStringExpander.getInstance(widgetElement.getAttribute("model"));
+            this.styleExdr = FlexibleStringExpander.getInstance(widgetElement.getAttribute("style"));
+        }
+
+        @Override
+        public void renderWidgetString(Appendable writer,
+                Map<String, Object> context,
+                ScreenStringRenderer screenStringRenderer)
+                throws GeneralException, IOException {
+            writer.append(this.rawString());
+        }
+
+        @Override
+        public String rawString() {
+            StringBuilder builder = new StringBuilder();
+            builder.append("<label class=\"checkbox\">");
+            builder.append("<input type=\"checkbox\" class=\"" + this.styleExdr.getOriginal() + "\" ng-model=\"" + this.modelExdr.getOriginal() + "\"/>");
+            builder.append(this.textExdr.getOriginal());
+            builder.append("</label>");
+            return builder.toString();
+        }
+        
+    }
+
+    @SuppressWarnings("serial")
+    public static class Collapse extends ModelScreenWidget {
+        public static final String TAG_NAME = "collapse";
+
+        protected FlexibleStringExpander collapseExdr;
+        protected List<ModelScreenWidget> subWidgets;
+        
+        public Collapse(ModelScreen modelScreen, Element widgetElement) {
+            super(modelScreen, widgetElement);
+            this.collapseExdr = FlexibleStringExpander.getInstance(widgetElement.getAttribute("collapse"));
+            // read sub-widgets
+            List<? extends Element> subElementList = UtilXml.childElementList(widgetElement);
+            this.subWidgets = ModelScreenWidget.readSubWidgets(this.modelScreen, subElementList);
+        }
+
+        @Override
+        public void renderWidgetString(Appendable writer,
+                Map<String, Object> context,
+                ScreenStringRenderer screenStringRenderer)
+                throws GeneralException, IOException {
+            writer.append(this.rawString());
+            renderSubWidgetsString(this.subWidgets, writer, context, screenStringRenderer);
+            writer.append("</div>");
+        }
+
+        @Override
+        public String rawString() {
+            return "<div collapse=\"" + this.collapseExdr.getOriginal() + "\">";
+        }
     }
 
     @SuppressWarnings("serial")
@@ -147,6 +288,28 @@ public class AngularJsScreenWidget {
             return "<span current-time=\"\"></span>";
         }
         
+    }
+
+    @SuppressWarnings("serial")
+    public static class HorizontalRule extends ModelScreenWidget {
+        public static final String TAG_NAME = "horizontal-rule";
+        
+        public HorizontalRule(ModelScreen modelScreen, Element widgetElement) {
+            super(modelScreen, widgetElement);
+        }
+
+        @Override
+        public void renderWidgetString(Appendable writer,
+                Map<String, Object> context,
+                ScreenStringRenderer screenStringRenderer)
+                throws GeneralException, IOException {
+            writer.append(this.rawString());
+        }
+
+        @Override
+        public String rawString() {
+            return "<hr/>";
+        }
     }
 
     @SuppressWarnings("serial")
