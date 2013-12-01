2fe009480f5d453f2d115d5f8f5a85fbd3370720
chatree.srichart@gmail.com <chatree.srichart@gmail.com@7281f183-68a6-c996-e254-01dca9f16df4>
2013-12-01 05:34:25 +0000

    initial ExampleCommonDecoratorController

---
+++ script-js/org/ofbiz/angularjs/example/controller/ExampleCommonDecoratorController.js
@@ -0,0 +1,16 @@
+package org.ofbiz.angularjs.example.controller;
+
+function ExampleCommonDecoratorController($scope) {
+    $scope.alerts = [
+                     { type: 'error', msg: 'Oh snap! Change a few things up and try submitting again.' }, 
+                     { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
+                 ];
+                 
+    $scope.addAlert = function() {
+        $scope.alerts.push({msg: "Another alert!"});
+    };
+    
+    $scope.closeAlert = function(index) {
+        $scope.alerts.splice(index, 1);
+    };
+}
