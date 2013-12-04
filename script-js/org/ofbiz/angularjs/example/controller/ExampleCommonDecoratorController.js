8705020b3b4596f463393ead27368108797b74e5
chatree.srichart@gmail.com <chatree.srichart@gmail.com@7281f183-68a6-c996-e254-01dca9f16df4>
2013-12-04 11:49:43 +0000

    clear response message whenever state is changed.

--- script-js/org/ofbiz/angularjs/example/controller/ExampleCommonDecoratorController.js
+++ script-js/org/ofbiz/angularjs/example/controller/ExampleCommonDecoratorController.js
@@ -3,7 +3,11 @@ package org.ofbiz.angularjs.example.controller;
 function ExampleCommonDecoratorController($rootScope, $scope) {
     
     $rootScope.$on("ON_HTTP_RESPONSE_MESSAGE_RECEIVED", function(event, responseMessage) {
-        $scope.alerts = [responseMessage];
+        // $scope.alerts = [responseMessage];
+    });
+    
+    $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+        $scope.alerts = [];
     });
     
     $scope.closeAlert = function(index) {
