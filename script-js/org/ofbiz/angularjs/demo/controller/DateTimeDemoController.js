7da1742ff49086060797e2b0279fbe9916d673e1
chatree.srichart@gmail.com <chatree.srichart@gmail.com@7281f183-68a6-c996-e254-01dca9f16df4>
2014-08-13 12:19:19 +0000

    show timestamp on date-time demo screen

--- script-js/org/ofbiz/angularjs/demo/controller/DateTimeDemoController.js
+++ script-js/org/ofbiz/angularjs/demo/controller/DateTimeDemoController.js
@@ -8,6 +8,8 @@ function DateTimeDemoController($scope, DateTimeUtil) {
     $scope.date = DateTimeUtil.getTime("2014-04-14 22:52:37.787");
 
     $scope.$watch("date", function(newVal) {
-        console.log("selected date: " + DateTimeUtil.toTimestamp(newVal));
+        if (newVal != null) {
+            $scope.timestamp = DateTimeUtil.toTimestamp(newVal);
+        }
     });
 }
