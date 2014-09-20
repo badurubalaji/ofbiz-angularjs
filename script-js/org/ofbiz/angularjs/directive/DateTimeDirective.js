dbdedd0f26d788cda7e49346485a34deb143947d
chatree.srichart@gmail.com <chatree.srichart@gmail.com@7281f183-68a6-c996-e254-01dca9f16df4>
2014-09-20 17:31:33 +0000

    DateTimeDirective, don't set empty default date time

--- script-js/org/ofbiz/angularjs/directive/DateTimeDirective.js
+++ script-js/org/ofbiz/angularjs/directive/DateTimeDirective.js
@@ -7,42 +7,46 @@ function DateTimeDirective($rootScope, $timeout, $compile, DateTimeUtil) {
         var style = $attrs.style;
         var format = $attrs.format;
 
-        $scope.seconds = 0;
+        $scope.seconds = null;
 
         var divElement = angular.element("<div class=\"ade-editable\" ade-date='{\"className\": \"" + style
                 + "\", \"format\": \"" + format + "\", \"absolute\": true}' ng-model=\"seconds\">{{seconds | validDate:['" + format + "']}}</div>");
         $element.html(divElement);
 
         $scope.$watch("ngModel", function(newVal) {
-            if (!$scope.isCallback) {
-                $scope.seconds = DateTimeUtil.getTime(newVal);
-            } else {
-                $scope.isCallback = false;
+            if (newVal != null) {
+                if (!$scope.isCallback) {
+                    $scope.seconds = DateTimeUtil.getTime(newVal);
+                } else {
+                    $scope.isCallback = false;
+                }
             }
         });
 
         $scope.$watch("seconds", function(newVal) {
-            $scope.isCallback = true;
-            var dateSeconds = newVal;
+            if (newVal != null) {
+                $scope.isCallback = true;
+                var dateSeconds = newVal;
 
-            // try to get current time
-            /*
-            var currentDate = new Date();
-            var seconds = currentDate.getSeconds();
-            var minutes = currentDate.getMinutes();
-            var hour = currentDate.getHours();
-            var timeSeconds = ((hour * 60 * 60) + (minutes * 60) + seconds);
-            */
+                // try to get current time
+                /*
+                var currentDate = new Date();
+                var seconds = currentDate.getSeconds();
+                var minutes = currentDate.getMinutes();
+                var hour = currentDate.getHours();
+                var timeSeconds = ((hour * 60 * 60) + (minutes * 60) + seconds);
+                */
 
-            var totalSeconds = dateSeconds;
-            $scope.ngModel = DateTimeUtil.toTimestamp(totalSeconds);
-            if(!$scope.$$phase) {
-                try {
-                    $timeout(function() {
-                        $scope.$apply();
-                    });
-                } catch (e) {
-                    // ignore Error: [$rootScope:inprog]
+                var totalSeconds = dateSeconds;
+                $scope.ngModel = DateTimeUtil.toTimestamp(totalSeconds);
+                if(!$scope.$$phase) {
+                    try {
+                        $timeout(function() {
+                            $scope.$apply();
+                        });
+                    } catch (e) {
+                        // ignore Error: [$rootScope:inprog]
+                    }
                 }
             }
         });
