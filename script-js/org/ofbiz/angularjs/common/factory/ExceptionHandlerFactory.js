68ca76270da9c108379ff741324a535cb708d3f6
chatree.srichart@gmail.com <chatree.srichart@gmail.com@7281f183-68a6-c996-e254-01dca9f16df4>
2014-10-18 16:36:35 +0000

    override $exceptionHandler factory

---
+++ script-js/org/ofbiz/angularjs/common/factory/ExceptionHandlerFactory.js
@@ -0,0 +1,12 @@
+package org.ofbiz.angularjs.common.factory;
+
+function ExceptionHandlerFactory() {
+    return function (exception, cause) {
+        if (exception.message.indexOf("[$rootScope:inprog]") > -1) {
+            console.warn("Action already in progress.");
+        } else {
+            exception.message += ' (caused by "' + cause + '")';
+            throw exception;
+        }
+      };
+}
